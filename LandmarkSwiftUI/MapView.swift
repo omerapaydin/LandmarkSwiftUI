cfeb0bc8743d9b6ecbd5ddf6d5d2ef9fd4400e69
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-27 02:17:05 +0300

    MapView

--- LandmarkSwiftUI/MapView.swift
+++ LandmarkSwiftUI/MapView.swift
@@ -6,13 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MapView: UIViewRepresentable {
+   
+    
+    var coordinate: CLLocationCoordinate2D
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
     }
 }
 
 #Preview {
-    MapView()
+    MapView(coordinate: landmarkArray[0].locationCoordinates)
 }
