fd1ac47b8af1e690f3b8d844b7007e01a24394e7
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-27 01:29:05 +0300

    Initial Commit

---
+++ LandmarkSwiftUI/LandmarkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkSwiftUIApp.swift
+//  LandmarkSwiftUI
+//
+//  Created by Ömer on 27.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarkSwiftUITests/LandmarkSwiftUITests.swift
@@ -0,0 +1,19 @@
+//
+//  LandmarkSwiftUITests.swift
+//  LandmarkSwiftUITests
+//
+//  Created by Ömer on 27.04.2026.
+//
+
+import Testing
+@testable import LandmarkSwiftUI
+
+struct LandmarkSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
