3b8aaaaf432759b092ab18706426efcf5ac401bc
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-27 02:42:47 +0300

    ContentView Image

--- LandmarkSwiftUI/ContentView.swift
+++ LandmarkSwiftUI/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
         VStack {
            MapView(coordinate: londonBridgeLandmark.locationCoordinates)
                 .frame(height: UIScreen.main.bounds.height*0.3)
+            
+            CircleImage(image: Image(londonBridgeLandmark.imageName))
+                .frame(width: UIScreen.main.bounds.width*0.9, height:UIScreen.main.bounds.height*0.3 )
+                .offset(y:UIScreen.main.bounds.height * -0.15)
         }
        
     }
