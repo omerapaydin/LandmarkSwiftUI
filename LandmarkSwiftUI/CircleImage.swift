0912a0553fc81e79111cd3435347a768f2165d7d
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-27 02:37:27 +0300

    CircleImage

---
+++ LandmarkSwiftUI/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  LandmarkSwiftUI
+//
+//  Created by Ömer on 27.04.2026.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
