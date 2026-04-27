e47482b659bc0e90ff51e31656eed8eb95904eac
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-27 17:23:22 +0300

    Tasarım düzenleni

--- LandmarkSwiftUI/ContentView.swift
+++ LandmarkSwiftUI/ContentView.swift
@@ -11,11 +11,33 @@ struct ContentView: View {
     var body: some View {
         VStack {
            MapView(coordinate: londonBridgeLandmark.locationCoordinates)
+                .edgesIgnoringSafeArea(.top)
                 .frame(height: UIScreen.main.bounds.height*0.3)
             
             CircleImage(image: Image(londonBridgeLandmark.imageName))
                 .frame(width: UIScreen.main.bounds.width*0.9, height:UIScreen.main.bounds.height*0.3 )
                 .offset(y:UIScreen.main.bounds.height * -0.15)
+                .padding(.bottom,-150)
+            
+            
+            VStack(alignment:.leading){
+             Text(londonBridgeLandmark.name)
+                    .font(.largeTitle)
+                    .foregroundColor(.orange)
+                
+                HStack{
+                    Text(londonBridgeLandmark.country)
+                        .font(.subheadline)
+                    
+                    Spacer()
+                    
+                    Text(londonBridgeLandmark.category)
+                        .font(.subheadline)
+                }
+                
+                
+            }.padding()
+            Spacer()
         }
        
     }
