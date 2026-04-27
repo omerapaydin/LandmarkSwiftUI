1c56c62f407ab56e7949f4b4e7173722c2c5b4b1
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-27 17:50:11 +0300

    LandmarkListView

---
+++ LandmarkSwiftUI/LandmarkListView.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkListView.swift
+//  LandmarkSwiftUI
+//
+//  Created by Ömer on 27.04.2026.
+//
+
+import SwiftUI
+
+struct LandmarkListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkListView()
+}
